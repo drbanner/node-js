623c46aefaaee3b6ce61b048036c973e3c4b3bc7
Sai Prashanth <sai.alekanti@gmail.com>
2019-06-16 19:19:05 -0400

    node js templete

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const superheroes = require('superheroes');
+ 
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+console.log(superheroes.random());
+
+const pokemon = require('pokemon');
+
+console.log(pokemon.all());console.log(pokemon.random());
